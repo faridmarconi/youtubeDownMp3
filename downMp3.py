bb5fa00b52076a309a46507865f62b9caf20b85f
Farid <faridmarconi@gmail.com>
2022-03-16 17:05:41 -0300

    download mp3 link youtube

---
+++ downMp3.py
@@ -0,0 +1,16 @@
+import youtube_dl
+
+def download_ytvid_as_mp3():
+    video_url = open("download MP3\links_mp3.txt", "r")
+    for line in video_url:
+        video_info = youtube_dl.YoutubeDL().extract_info(url = line,download=False)
+        filename = f"{video_info['title']}.mp3"
+        options={
+            'format':'bestaudio/best',
+            'keepvideo':False,
+            'outtmpl':filename,
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+        print("Download complete... {}".format(filename))
+download_ytvid_as_mp3()
